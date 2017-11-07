870fbed2393a7a84458e2867566e128f3083924f
AndreyKonovalenko <andreinew@mail.ru>
2017-11-07 18:46:28 +0300

    users sign up

--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,31 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import uniqueValidator from 'mongoose-unique-validator'
 
 /* add add uniqueness and eameil validations to email field */
 
 const schema = new mongoose.Schema(
     {
-        email: { type: String, required: true, lowercase: true, index: true },
-        passwordHash: { type: String, required: true }
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            index: true,
+            unique: true
+        },
+        passwordHash: { type: String, required: true },
+        comfirmed: { type: Boolean, default: true }
     },
     { timestamps: true }
 )
 
-schema.methods.isValidPassword = function (password) {
+schema.methods.isValidPassword = function isValidPassword (password) {
     return bcrypt.compareSync(password, this.passwordHash)
+}
+
+schema.methods.setPassword = function setPassword (password) {
+    this.passwordHash = bcrypt.hashSync(password, 10)
 }
 
 schema.methods.generateJWT = function generateJWT() {
@@ -27,8 +39,11 @@ schema.methods.generateJWT = function generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON () {
     return {
         email: this.email,
+        comfirmed: this.comfirmed,
         token: this.generateJWT()
     }
 }
+
+schema.plugin(uniqueValidator, {massage: 'This email is allready taken'})
 
 export default mongoose.model('User',  schema)
